56a14995aab2b6f60d008c48afa02baf12e6f5ad
AndyM <andym@whatcomtrans.net>
2017-05-09 16:43:17 -0700

    Initial push from Andy
    - Modal items don't do anything yet

--- README.md
+++
@@ -1,3 +0,0 @@
-# aimwebgui
-
-Provides a web app for quickly changing what AdderLink receivers are receiving via the Adder AIM server REST API.
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Monitor Configuration'});
+});
+
+router.get('/:id', function(req, res, next) {
+	//server side logic here
+	var id = req.params.id;
+	res.render('index', {user: id, title: 'Monitor Configuration'});
+});
+
+module.exports = router;
