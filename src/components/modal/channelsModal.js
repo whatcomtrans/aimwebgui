08eb12dbf54e3120b3371d88f03994fa9ef5f145
danielm-ridewta <danielm@whatcomtrans.net>
2020-06-17 07:21:34 -0700

    Show availalbe and in use channels

--- src/components/modal/channelsModal.js
+++ src/components/modal/channelsModal.js
@@ -8,38 +8,65 @@ const ChannelsModal = ({
   channelUsageMap,
   closeModal,
   receiver,
-  selectChannel
-}) => (
-  <Modal isOpen={isOpen} closeModal={closeModal}>
-    <div className={styles.header}>
-      <div>Channels</div>
-      <button className={styles.closeButton} onClick={closeModal}>
-        X
-      </button>
-    </div>
-    <div>
-      {channels &&
-        channels.map(channel => (
-          <div
-            className={
-              channel.c_id === (receiver && receiver.channelId)
-                ? styles.selectedChannel
-                : styles.channel
-            }
-            onClick={() => {
-              selectChannel(channel.c_id, receiver.deviceId);
-            }}
-            key={channel.c_id}
-          >
-            {`${channel.c_description} ${
-              channelUsageMap[channel.c_name]
-                ? "(" + channelUsageMap[channel.c_name] + ")"
-                : ""
-            }`}
+  selectChannel,
+}) => {
+  let availableChannels = channels.filter(
+    (channel) => !channelUsageMap[channel.c_name]
+  );
+  let inUseChannels = channels.filter(
+    (channel) => channelUsageMap[channel.c_name]
+  );
+
+  return (
+    <Modal isOpen={isOpen} closeModal={closeModal}>
+      <div className={styles.header}>
+        <div>Channels</div>
+        <button className={styles.closeButton} onClick={closeModal}>
+          X
+        </button>
+      </div>
+      <div className={styles.channelsList}>
+        <div className={styles.availableChannels}>
+          <div className={styles.channelStatus}>Available</div>
+          <div>
+            {availableChannels.map((channel) => (
+              <div
+                className={
+                  channel.c_id === (receiver && receiver.channelId)
+                    ? styles.selectedChannel
+                    : styles.channel
+                }
+                onClick={() => {
+                  selectChannel(channel.c_id, receiver.deviceId);
+                }}
+                key={channel.c_id}
+              >
+                {channel.c_description}
+              </div>
+            ))}
           </div>
-        ))}
-    </div>
-  </Modal>
-);
+        </div>
+        <div className={styles.inUseChannels}>
+          <div className={styles.channelStatus}>In Use</div>
+          {inUseChannels.map((channel) => (
+            <div
+              className={
+                channel.c_id === (receiver && receiver.channelId)
+                  ? styles.selectedChannel
+                  : styles.channel
+              }
+              onClick={() => {
+                selectChannel(channel.c_id, receiver.deviceId);
+              }}
+              key={channel.c_id}
+            >
+              {`${channel.c_description} (${channelUsageMap[channel.c_name]})`}
+            </div>
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 export default ChannelsModal;
